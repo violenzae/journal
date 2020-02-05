f68729ec6c1a6d1efef7466fee316004fa2115de
Rachel Schieferstein <violenza@gmail.com>
2020-02-04 20:41:08 -0800

    separated business and front end logic, imported the object and finished the methods for string analysis

--- src/journal.js
+++ src/journal.js
@@ -1,13 +1,34 @@
 
-
-
-export function Entry () {
+export function Entry(subject, body) {
   this.subject = subject;
   this.body = body;
 }
 
 Entry.prototype.wordCount = function() {
-  var words = this.body.toLowerCase().split(" ");
+  var words = this.body.split(" ");
   return words.length;
-};
+}
 
+Entry.prototype.voweler = function() {
+  var vowel_list = "aeiouAEIOU"
+  var vcount = 0
+  for(var i = 0; i < this.body.length ; i++) {
+    if(vowel_list.indexOf(this.body[i]) !== -1)
+     {
+      vcount += 1
+    } 
+  }
+    return vcount;
+  }
+
+Entry.prototype.consonate = function() {
+  var conso_list = "bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ"
+  var ccount = 0
+  for (var i=0; i < this.body.length; i++) {
+    if(conso_list.indexOf(this.body[i]) !== -1)
+    {
+      ccount += 1
+    }
+  }
+  return ccount;
+}
--- src/main.js
+++ src/main.js
@@ -1,20 +1,7 @@
 
 
-//  import { Entry } from './journal';
-//  import './styles.css';
-
-
-
- function Entry(subject, body) {
-  this.subject = subject;
-  this.body = body;
-}
-
-Entry.prototype.wordCount = function() {
-  var words = this.body.split(" ");
-  return words.length;
-}
-
+ import { Entry } from './journal';
+ import './styles.css';
 
  
 $(document).ready(function() {
@@ -24,14 +11,14 @@ $(document).ready(function() {
     var body = $('#body').val();
     var entry = new Entry(subject, body);
     console.log(entry);
-    var words = [];
+    var vowels = entry.voweler();
     var wordCount = entry.wordCount();
-
+    var consonants = entry.consonate();
  
 
-    console.log(wordCount);
-  
-    $('#solution').text(words);
+    console.log("word count: "+wordCount);
+    console.log("consonants: "+consonants);
+    $('#post').append('<div><p>'+subject+'</p><p>'+body+'</p><br><p>word count: '+wordCount+' vowels: '+vowels+' = consonants: '+consonants+'</div>');
 
     // output.forEach(function(element) {
     //   $('#solution').append("<li>" + element + "</li>");
